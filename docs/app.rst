77237f20e4fe6d420ccbf6df2a095e18e0005050
Fabian Schuh <mail@xeroc.org>
2016-05-17 13:38:28 +0200

    [docs] initial commit for documentation

---
+++ docs/app.rst
@@ -0,0 +1,225 @@
+Piston: The App for Steem
+=========================
+
+Swiss army knife for interacting with the STEEM blockchain.
+
+Adding keys (for posting)
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Piston comes with its own encrypted wallet to which keys need to be
+added:
+
+::
+
+    piston addkey <posting-wif-key>
+
+On first run, you will be asked to provide a new passphrase that you
+will need to provide every time you want to post on the Steem network.
+If you chose an *empty* password, your keys will be stored in plain text
+which allows automated posting but exposes your private key to your
+local user.
+
+List available Keys and accounts
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+::
+
+    piston listkeys
+
+This command will give the list of public keys to which the private keys
+are available.
+
+::
+
+    piston listaccounts
+
+This command tries to resolve the public keys into account names
+registered on the network (experimental).
+
+Configuration
+~~~~~~~~~~~~~
+
+``piston`` comes with its owne configuration:
+
+::
+
+    piston set default_voter <account-name>
+    piston set default_author <account-name>
+
+All configuration variables are provided with ``piston set --help``
+
+Listing
+~~~~~~~
+
+``piston`` can list, sort, and filter for posts on the STEEM blockchain.
+You can read about the parameters by
+
+::
+
+    piston list --help
+
+Example:
+
+::
+
+    $ piston list --limit 3 --sort payout
+    +----------------------------------------+------------------------------------------------+----------+---------+------------------+---------------+
+    | identifier                             | title                                          | category | replies |            votes |       payouts |
+    +----------------------------------------+------------------------------------------------+----------+---------+------------------+---------------+
+    | @donaldtrump/lets-talk-politics        | Let's Talk Politics and the U.S. 2016 Election | politics |    20   | 1020791260074419 | 14106.752 SBD |
+    | @nextgencrypto/steem-price-speculation | STEEM Price Speculation                        | steem    |    14   |  777027533714240 | 11675.872 SBD |
+    | @clayop/lets-request-steem-to-poloniex | Let's Request STEEM to Poloniex                | steem    |    8    |  988929602909199 | 10530.426 SBD |
+    +----------------------------------------+------------------------------------------------+----------+---------+------------------+---------------+
+
+Reading
+~~~~~~~
+
+The subcommand ``read`` allows to read posts and replies from STEEM by
+providing the post *identifier*. The identifier takes the form
+
+::
+
+    @author/permlink
+
+The subcommands takes the optional parameters:
+
+-  ``--full``: show the posts meta data as YAML formatted frontmatter
+-  ``--comments``: to show all comments and replies made to that post
+-  ``--parents x``: Show ``x`` parent posts
+
+See examples:
+
+::
+
+    $ piston read "@xeroc/piston-readme"
+
+    [this readme]
+
+    $ piston read "@xeroc/python-steem-0-1" --comments
+
+     ---
+     author: puppies
+     permlink: re-python-steem-0-1
+     reply: '@puppies/re-python-steem-0-1'
+     ---
+
+     Great work Xeroc.  Your libraries make working with graphene chains truly a joy.
+       ---
+       author: xeroc
+       permlink: re-puppies-re-python-stem-0-1
+       reply: '@xeroc/re-puppies-re-python-stem-0-1'
+       ---
+       
+       Thank you, I enjoy writing python a lot myself!
+     ---
+     author: dantheman
+     permlink: re-xeroc-python-steem-0-1-20160414t145522693z
+     reply: '@dantheman/re-xeroc-python-steem-0-1-20160414t145522693z'
+     ---
+
+     This is great work xeroc!  Thanks for supporting steem!
+
+Categories
+~~~~~~~~~~
+
+Existing categories can be listed via:
+
+::
+
+    piston categories --limit 10
+
+Please see the corresponding help page for further options:
+
+::
+
+    piston categories --help
+
+Posting
+~~~~~~~
+
+To post new content, you need to provide
+
+-  the author,
+-  a permlink, and
+-  a title
+
+For posting the "posting-key" of the author needs to be added to the
+wallet.
+
+Additionally, a ``--category`` can be added as well.
+
+::
+
+    echo "Texts" | piston post --author "<author>" --category "<category>" --title "<posttitle>" --permlink "<permlink>"
+    cat filename | piston post --author "<author>" --category "<category>" --title "<posttitle>" --permlink "<permlink>"
+
+Replying
+~~~~~~~~
+
+Here, the same parameters as for simply posting new content are
+available except that instead of ``--category`` a ``replyto`` has to be
+provided to identify the post that you want the reply to be posted to.
+The ``replyto`` parameter takes the following form:
+
+::
+
+    @author/permlink
+
+E.g:
+
+::
+
+    echo "Texts" | piston reply --file - "@xeroc/python-steem-0.1.1" --author "<author>"
+    cat filename | piston reply --file - "@xeroc/python-steem-0.1.1" --author "<author>"
+
+If you want to use your favorit ``EDITOR``, you can do this by:
+
+::
+
+    piston reply "@xeroc/python-steem-0.1.1"
+
+Editing
+~~~~~~~
+
+With piston, you can edit your own posts with your favorite text editor
+(as defined in the environmental variable ``EDITOR``):
+
+::
+
+    $ piston "@xeroc/edit-test" 
+    $ EDITOR="nano" piston "@xeroc/edit-test" 
+
+If you want to replace your entire post and not *patch* it, you can add
+the ``--replace`` flag.
+
+Voting
+~~~~~~
+
+With ``piston``, you can up-/downvote any post with your installed
+accounts:
+
+::
+
+    piston upvote --voter <voter> <identifier>
+    piston downvote --voter <voter> <identifier>
+
+providing the post *identifier*. The identifier takes the form
+
+::
+
+    @author/permlink
+
+You can further define the weight (default 100%) manually with
+``--weight``.
+
+Replies
+~~~~~~~
+
+``piston`` can show replies to posts made by any author:
+
+::
+
+    piston replies --author xeroc
+
+If ``--author`` is not provided, the *default* author as defined with
+``piston set author`` will be taken. Further options are: ``--limit``.
--- piston/steem.py
+++ piston/steem.py
@@ -16,7 +16,33 @@ config = Configuration()
 if "node" not in config or not config["node"]:
     config["node"] = "wss://steemit.com/ws"
 
+
 class Steem(object):
+    """ The purpose of this class it to simplify posting and dealing
+        with accounts, posts and categories in Steem.
+
+        The idea is to have a class that allows to do this:
+
+        .. code-block:: python
+
+            from piston.steem import Steem
+            steem = Steem()
+            steem.post("Testing piston-libs", "I am testing piston-libs", category="spam")
+
+        All that is requires is for the user to have added a posting key with
+
+        .. code-block:: bash
+
+            piston addkey
+
+        and setting a default author:
+
+        .. code-block:: bash
+
+            piston set default_author xeroc
+
+        This class also deals with edits, votes and reading content.
+    """
 
     def __init__(self, *args, **kwargs):
         self.connect(*args, **kwargs)
@@ -33,11 +59,11 @@ class Steem(object):
             SteemIT.com. It is **highly** recommended that you pick your own
             node instead. Default settings can be changed with:
 
-            ```
-            piston set node <host>
-            ```
+            .. code-block:: python
 
-            where `<host>` starts with `ws://` or `wss://`.
+                piston set node <host>
+
+            where ``<host>`` starts with ``ws://`` or ``wss://``.
         """
         self.nobroadcast = nobroadcast
         if not node:
@@ -54,13 +80,12 @@ class Steem(object):
 
         self.rpc = SteemNodeRPC(node, rpcuser, rpcpassword)
 
-
     def executeOp(self, op, wif=None):
-        """ Execute an operation by signing it with the `wif` key and
+        """ Execute an operation by signing it with the ``wif`` key and
             broadcasting it to the Steem network
 
             :param Object op: The operation to be signed and broadcasts as
-                              provided by the `transactions` class.
+                              provided by the ``transactions`` class.
             :param string wif: The wif key to use for signing a transaction
 
             **TODO**: The full node could, given the operations, give us a
@@ -97,33 +122,31 @@ class Steem(object):
 
         return tx
 
-
     def reply(self, identifier, body, title="", author="", meta=None):
         """ Reply to an existing post
 
             :param str identifier: Identifier of the post to reply to. Takes the
-                             form `@author/permlink`
+                             form ``@author/permlink``
             :param str body: Body of the reply
             :param str title: Title of the reply post
             :param str author: Author of reply (optional) if not provided
-                               `default_user` will be used, if present, else
-                               a `ValueError` will be raised.
+                               ``default_user`` will be used, if present, else
+                               a ``ValueError`` will be raised.
             :param json meta: JSON meta object that can be attached to the
                               post. (optional)
         """
         self.post(title, body, meta=meta, author=author, reply_identifier=identifier)
 
-
     def edit(self, identifier, body, meta=None, replace=False):
         """ Edit an existing post
 
             :param str identifier: Identifier of the post to reply to. Takes the
-                             form `@author/permlink`
+                             form ``@author/permlink``
             :param str body: Body of the reply
             :param json meta: JSON meta object that can be attached to the
                               post. (optional)
             :param bool replace: Instead of calculating a *diff*, replace
-                                 the post entirely (defaults to `False`)
+                                 the post entirely (defaults to ``False``)
         """
         post_author, post_permlink = resolveIdentifier(identifier)
         original_post = self.rpc.get_content(post_author, post_permlink)
@@ -145,13 +168,14 @@ class Steem(object):
             original_post["parent_permlink"]
         )
 
-        self.post(original_post["title"],
-             newbody,
-             reply_identifier=reply_identifier,
-             author=original_post["author"],
-             permlink=original_post["permlink"],
-             meta=original_post["json_metadata"])
-
+        self.post(
+            original_post["title"],
+            newbody,
+            reply_identifier=reply_identifier,
+            author=original_post["author"],
+            permlink=original_post["permlink"],
+            meta=original_post["json_metadata"]
+        )
 
     def post(self,
              title,
@@ -166,15 +190,15 @@ class Steem(object):
             :param str title: Title of the reply post
             :param str body: Body of the reply
             :param str author: Author of reply (optional) if not provided
-                               `default_user` will be used, if present, else
-                               a `ValueError` will be raised.
+                               ``default_user`` will be used, if present, else
+                               a ``ValueError`` will be raised.
             :param json meta: JSON meta object that can be attached to the
                               post.
             :param str reply_identifier: Identifier of the post to reply to. Takes the
-                                         form `@author/permlink`
+                                         form ``@author/permlink``
             :param str category: Allows to define a category for new posts.
                                  It is highly recommended to provide a
-                                 category as posts end up in `spam`
+                                 category as posts end up in ``spam``
                                  otherwise.
         """
 
@@ -214,22 +238,21 @@ class Steem(object):
         wif = Wallet(self.rpc).getPostingKeyForAccount(author)
         self.executeOp(op, wif)
 
-
     def vote(self, identifier, weight, voter=None):
         """ Vote for a post
 
             :param str identifier: Identifier for the post to upvote Takes
-                                   the form `@author/permlink`
+                                   the form ``@author/permlink``
             :param float weight: Voting weight. Range: -100.0 - +100.0. May
                                  not be 0.0
             :param str voter: Voter to use for voting. (Optional)
-            
-            If `voter` is not defines, the `default_voter` will be taken or
+
+            If ``voter`` is not defines, the ``default_voter`` will be taken or
             a ValueError will be raised
 
-            ```
-            piston set default_voter <account>
-            ```
+            .. code-block:: python
+
+                piston set default_voter <account>
         """
 
         STEEMIT_100_PERCENT = 10000
@@ -252,16 +275,14 @@ class Steem(object):
         wif = Wallet(self.rpc).getPostingKeyForAccount(voter)
         self.executeOp(op, wif)
 
-
     def get_content(self, identifier):
         """ Get the full content of a post.
 
             :param str identifier: Identifier for the post to upvote Takes
-                                   the form `@author/permlink`
+                                   the form ``@author/permlink``
         """
         post_author, post_permlink = resolveIdentifier(identifier)
         return self.rpc.get_content(post_author, post_permlink)
-
 
     def get_replies(self, author, skipown=True):
         """ Get replies for an author
@@ -279,18 +300,17 @@ class Steem(object):
             discussions.append(post)
         return discussions
 
-
     def get_posts(self, limit=10,
                   sort="recent",
                   category=None,
                   start=None,):
         """ Get multiple posts in an array.
 
-            :param int limit: Limit the list of posts by `limit`
+            :param int limit: Limit the list of posts by ``limit``
             :param str sort: Sort the list by "recent" or "payout"
             :param str category: Only show posts in this category
             :param str start: Show posts after this post. Takes an
-                              identifier of the form `@author/permlink`
+                              identifier of the form ``@author/permlink``
         """
         from functools import partial
         if sort == "recent":
@@ -314,14 +334,13 @@ class Steem(object):
 
         return func(author, permlink, limit)
 
-
     def get_categories(self, sort, begin="", limit=10):
         """ List categories
 
             :param str sort: Sort categories by "trending", "best",
                              "active", or "recent"
             :param str begin: Show categories after this
-            :param int limit: Limit categories by `x`
+            :param int limit: Limit categories by ``x``
         """
         if sort == "trending":
             func = self.rpc.get_trending_categories
